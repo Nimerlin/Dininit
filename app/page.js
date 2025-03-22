52403ffa24a08884b7647c608620649d491f5e0b
Nitin <nitin@gmail.com>
2025-03-22 14:41:53 +0530

    update landing syntax

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 // pages/index.js
 import React from 'react';
 // import LandingPage from "../components/LandingPage";
-import LandingPage from './components/LandingPage/landingPage';
+import LandingPage from './components/landingPage/landingPage';
 
 export default function Home() {
   return <LandingPage />;
